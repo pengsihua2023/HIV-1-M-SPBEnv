63feead10cc7d99b9de77892bf27eac3cbb247eb
Sihua Peng <131550223+pengsihua2023@users.noreply.github.com>
2024-01-26 09:59:33 -0500

    Update tiqu_anjisuan_xulie.py

--- code/tiqu_anjisuan_xulie.py
+++ code/tiqu_anjisuan_xulie.py
@@ -1,27 +1,27 @@
 
- 
+# Extract the amino acid sequence except the start codon and stop codon
  
 import csv
 from Bio import SeqIO
 
-# 读取FASTA文件
+# Read FASTA files
 fasta_file = "L02317.fasta"
 records = SeqIO.parse(fasta_file, "fasta")
 
-# 提取密码子序列
+# Extract codon sequence
 codon_sequences = []
 for record in records:
     sequence = str(record.seq)
-    start_index = 3  # 起始密码子索引
-    end_index = len(sequence) - 3  # 终止密码子索引
+    start_index = 3  # Start codon index
+    end_index = len(sequence) - 3  # Stop codon index
     codon_sequence = [sequence[i:i+3] for i in range(start_index, end_index, 3)]
     codon_sequences.extend(codon_sequence)
 
-# 写入CSV文件
+#Write to CSV file
 csv_file = "L02317_codon_sequences.csv"
 with open(csv_file, mode="w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["序号", "密码子序列"])
+    writer.writerow(["serial number", "codon sequence"])
     for i, codon in enumerate(codon_sequences, start=1):
         writer.writerow([i, codon])
     
