14415b6a70c1b29db699c25d68b28cd1f50786cc
Sihua Peng <131550223+pengsihua2023@users.noreply.github.com>
2024-03-11 16:43:10 -0400

    Update Visual-with-t-SNE.py

--- code/Visual-with-t-SNE.py
+++ code/Visual-with-t-SNE.py
@@ -4,14 +4,14 @@ from Bio import SeqIO
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-# 读取FASTA文件中的序列，并返回序列及其ID
+# Read sequences from a FASTA file and return the sequence
 def read_fasta(file_path):
     sequences = []
     for record in SeqIO.parse(file_path, "fasta"):
         sequences.append(str(record.seq).upper())
     return sequences
 
-# 计算序列的k-mer频率
+# Calculate the k-mer frequency of a sequence
 def get_kmer_freqs(sequences, k):
     kmer_set = set()
     kmer_freqs = []
@@ -35,22 +35,22 @@ def get_kmer_freqs(sequences, k):
     
     return np.array(kmer_freqs)
 
-# 直接读取CSV文件中的标签
+# Read tags directly from CSV files
 def read_labels(file_path):
-    labels_df = pd.read_csv(file_path, header=None)  # 假设CSV文件没有列名
-    labels = labels_df[0].tolist()  # 假设标签在第一列
+    labels_df = pd.read_csv(file_path, header=None)  # Assuming the CSV file has no column names
+    labels = labels_df[0].tolist()  # Assume the label is in the first column
     return labels
 
-# 创建一个颜色映射字典，为每种标签分配不同的颜色
+# Create a color map dictionary to assign a different color to each type of label
 def get_color_map(labels):
     unique_labels = sorted(set(labels))
     colors = plt.cm.get_cmap('tab20', len(unique_labels))
     label_to_color = {label: colors(i) for i, label in enumerate(unique_labels)}
     return label_to_color
 
-# 主要流程
+# Main process
 sequences = read_fasta('sihua.fasta')
-labels = read_labels('Label.csv')  # 直接按顺序分配标签
+labels = read_labels('Label.csv')  # Assign tags directly in sequence
 kmer_freqs = get_kmer_freqs(sequences, k=6)
 
 tsne = TSNE(n_components=2, random_state=42)
